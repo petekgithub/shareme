c8f295952ac39c3430b2e706b1a03c4ac6d22014
Petech <petekshamitbeyli@gmail.com>
2024-01-17 22:25:05 +0300

    fe_setup

--- shareme_fe/src/App.js
+++ shareme_fe/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hello boss !</div>;
+  return <h1 className="text-3xl font-bold underline">Hello boss !</h1>;
 };
 
 export default App;
