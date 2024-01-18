e81304a82a70b3e0a4a641cba3abc6a9cf2a69ef
Petech <petekshamitbeyli@gmail.com>
2024-01-18 17:05:57 +0300

    signIn-w-google

--- shareme_fe/src/components/Login.jsx
+++ shareme_fe/src/components/Login.jsx
@@ -6,6 +6,8 @@ import shareVideo from "../assets/share.mp4";
 import logo from "../assets/sharelogo.png";
 
 const Login = () => {
+  const responseGoogle = (response) => {};
+
   return (
     <div className="flex justify-start items-center flex-col h-screen">
       <div className=" relative w-full h-full">
@@ -22,6 +24,24 @@ const Login = () => {
         <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
           <div className="p-5">
             <img src={logo} width="130px" alt="logo" />
+          </div>
+          <div className="shadow-2xl">
+            <GoogleLogin
+              clientId="YOUR_GOOGLE_CLIENT_ID"
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className="mr-4" /> Sign in with google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single_host_origin"
+            />
           </div>
         </div>
       </div>
--- shareme_fe/src/index.js
+++ shareme_fe/src/index.js
@@ -1,13 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(
+createRoot(document.getElementById("root")).render(
   <Router>
     <App />,
-  </Router>,
-  document.getElementById("root")
+  </Router>
 );
