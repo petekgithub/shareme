0ca091e59a4dd159b7a95ad26fcbcad80af9a983
Petech <petekshamitbeyli@gmail.com>
2024-01-19 11:14:27 +0300

    login-update

--- shareme_fe/src/components/Login.jsx
+++ shareme_fe/src/components/Login.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import GoogleLogin from "react-google-login";
+import { GoogleLogin } from "@react-oauth/google";
 import { useNavigate } from "react-router-dom";
-import { FcGoogle } from "react-icons/fc";
 import shareVideo from "../assets/share.mp4";
 import logo from "../assets/sharelogo.png";
 import { jwtDecode } from "jwt-decode";
@@ -13,6 +12,8 @@ const Login = () => {
 
   const responseGoogle = (response) => {
     localStorage.setItem("user", JSON.stringify(response.profileObj));
+
+    // Assuming response.credential contains the JWT token
     const decodedHeader = jwtDecode(response.credential);
 
     const { name, sub, picture } = decodedHeader;
@@ -28,30 +29,6 @@ const Login = () => {
     });
   };
 
-  // const responseGoogle = (response) => {
-  //   console.log("Google Login Response:", response);
-
-  //   if (response && response.profileObj) {
-  //     localStorage.setItem("user", JSON.stringify(response));
-  //     const { clientId, name, imageUrl } = response;
-  //     const doc = {
-  //       _id: clientId,
-  //       _type: "user",
-  //       userName: name,
-  //       image: imageUrl,
-  //     };
-  //     client.createIfNotExists(doc).then(() => {
-  //       navigate("/", { replace: true });
-  //     });
-  //   } else {
-  //     // Handle the case where profileObj is not present in the response
-  //     console.error(
-  //       "Google login failed. Missing profileObj in response.",
-  //       response
-  //     );
-  //   }
-  // };
-
   return (
     <div className="flex justify-start items-center flex-col h-screen">
       <div className=" relative w-full h-full">
@@ -65,7 +42,7 @@ const Login = () => {
           className="w-full h-full object-cover"
         />
 
-        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0    bg-blackOverlay">
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
           <div className="p-5">
             <img src={logo} width="130px" alt="logo" />
           </div>
@@ -73,16 +50,6 @@ const Login = () => {
           <div className="shadow-2xl">
             <GoogleLogin
               clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
-              render={(renderProps) => (
-                <button
-                  type="button"
-                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
-                  onClick={renderProps.onClick}
-                  disabled={renderProps.disabled}
-                >
-                  <FcGoogle className="mr-4" /> Sign in with google
-                </button>
-              )}
               onSuccess={responseGoogle}
               onError={responseGoogle}
               cookiePolicy="single_host_origin"
